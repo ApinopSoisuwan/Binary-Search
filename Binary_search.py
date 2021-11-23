e851f97f698718224b9af1371562a9297da57403
Apinop <apinop.soisuwan@gmail.com>
2021-11-23 16:05:50 +0700

    used index function

--- Binary_search.py
+++ Binary_search.py
@@ -1,13 +1,10 @@
 def search(nums,target):
-    c = 0
-    for i in nums:
-        c += 1
-        if i == target :
-            return c - 1
+    if target in nums:
+        return nums.index(target)
     return -1
 
 t1 = [-1,0,3,5,9,12]
 t2 = 9
 b1 = [-1,0,3,5,9,12]
 b2 = 2
-print(search(b1,b2))
+print(search(t1,t2))
